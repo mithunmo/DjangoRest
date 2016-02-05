2ef81c6fb1719b0b32f54e140554e0fe1b480ac0
Mithun <mithun.mohan@mofilm.com>
2016-02-05 12:27:32 +0530

    Adding testcase for portal

--- portal/migrations/0002_portalcontent_portaluser.py
+++ portal/migrations/0002_portalcontent_portaluser.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('movies', '0001_initial'),
         ('portal', '0001_initial'),
     ]
@@ -20,7 +22,15 @@ class Migration(migrations.Migration):
                 ('contentType', models.CharField(max_length=100)),
                 ('licenseTerms', models.TextField()),
                 ('createDate', models.DateTimeField(auto_now=True)),
-                ('movies', models.OneToOneField(to='movies.Movie')),
+                ('movies', models.OneToOneField(db_column=b'movies_id', to='movies.Movie')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='PortalUser',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, db_column=b'ID')),
+                ('portalID', models.OneToOneField(to='portal.Portal')),
+                ('userID', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
--- portal/tests.py
+++ portal/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
 # Create your tests here.
+# Create your tests here.
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+#from myproject.apps.core.models import Account
+from models import Portal
+
+class PortalTests(APITestCase):
+
+    """
+    def setUp(self):
+        Portal.objects.create(brandID=4, status="Enabled")
+        Portal.objects.create(brandID=5, status="Disabled")
+    """
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        #url = reverse('mofilmuser')
+        #data = {'name': 'DabApps'}
+        #response = self.client.post(url, data, format='json')
+        #response = self.client.get(url)
+        #print response
+        response = self.client.get('/v1/portal/1/')
+        print response
+        #self.assertEqual(response.data["ud"], {'id': 1, 'brandID': 4})
+        self.assertEqual(response.data["brandID"], 4)
+
+        """
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Account.objects.count(), 1)
+        self.assertEqual(Account.objects.get().name, 'DabApps')
+        """
